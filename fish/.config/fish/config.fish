7332bad85a7e3cef8d5caf049404b3aebadbc01a
denkl <kolomytskyi@gmail.com>
2021-08-18 23:21:06 +0300

    Add fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,41 @@
+if status is-interactive
+
+    set --global fish_greeting
+    set --global --export EDITOR vim
+
+    starship init fish | source
+
+    pyenv init --path | source
+    pyenv init - | source
+    pyenv virtualenv-init - | source
+    set --global --export PYENV_VIRTUALENV_DISABLE_PROMPT 1
+
+    fish_add_path $HOME/.cargo/env
+
+    function __history_previous_command
+        switch (commandline -t)
+            case "!"
+                commandline -t $history[1]; commandline -f repaint
+            case "*"
+                commandline -i !
+        end
+    end
+
+    function __history_previous_command_arguments
+        switch (commandline -t)
+            case "!"
+                commandline -t ""
+                commandline -f history-token-search-backward
+            case "*"
+                commandline -i '$'
+        end
+    end
+
+    bind ! __history_previous_command
+    bind '$' __history_previous_command_arguments
+
+    bind W nextd-or-forward-word
+
+    abbr --add --global e nvim
+
+end
