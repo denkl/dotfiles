64b90acce2683ee96ab9849867834c0ffdb80d16
Denys Kolomytskyi <kolomytskyi@gmail.com>
2021-11-18 11:33:55 +0200

    Add script for installing tools using brew

---
+++ brew.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env sh
+
+echo 'start...'
+
+brew update
+
+brew upgrade
+
+brew install bat
+brew install coreutils
+brew install dust
+brew install fd
+brew install fish
+brew install fzf
+brew install git
+brew install git-delta
+brew install gnupg
+brew install htop
+brew install jq
+brew install kubectl
+brew install ncdu
+brew install neovim
+brew install node
+brew install pv
+brew install pyenv
+brew install pyenv-virtualenv
+brew install stow
+brew install telnet
+brew install tldr
+brew install tokei
+brew install wget
+brew install youtube-dl
+
+brew install --cask kitty
+brew install --cask font-fira-code
+brew install --cask ngrok
+
+brew cleanup
+
+echo 'done'
