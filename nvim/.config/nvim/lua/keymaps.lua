1a3cc3011e3e594ca8b4f6b1cc1ad4132929f821
Denys Kolomytskyi <kolomytskyi@gmail.com>
2024-03-30 10:56:22 +0200

    Update keymaps

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -3,10 +3,13 @@ vim.keymap.set("n", "n", "nzzzv", { silent = true })
 vim.keymap.set("n", "N", "Nzzzv", { silent = true })
 vim.keymap.set("n", "<c-d>", "<c-d>zz")
 vim.keymap.set("n", "<c-u>", "<c-u>zz")
+vim.keymap.set("n", "g;", "g;zz")
+vim.keymap.set("n", "g,", "g,zz")
 
 -- preserve cursor position on join lines
 vim.keymap.set("n", "J", "mzJ`z")
 
+-- add blank line below cursor
 vim.keymap.set("n", "<cr>", "mzo<esc>k`z")
 
 -- undo break points
@@ -23,10 +26,19 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
 -- insert inside brackets
 vim.keymap.set("n", "<leader>i", "i<cr><esc>O")
 
--- window mappings
-vim.keymap.set("n", "<leader>w", "<c-w>w")
-vim.keymap.set("n", "<leader>q", "<cmd>close<cr>")
-
 -- buffer mappings
-vim.keymap.set("n", "]b", "<cmd>bnext<cr>zz")
-vim.keymap.set("n", "[b", "<cmd>bprevious<cr>zz")
+vim.keymap.set("n", "]b", "<cmd>bnext<cr>")
+vim.keymap.set("n", "[b", "<cmd>bprevious<cr>")
+vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<cr>")
+
+-- split navigation
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- diagnostic mappings
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
