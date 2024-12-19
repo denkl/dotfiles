f3c0807e8307f3c1444720454ef14474ef82e119
Denys Kolomytskyi <kolomytskyi@gmail.com>
2024-12-19 09:26:17 +0200

    Remove deleted folders

--- stow.sh
+++ stow.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
-STOW_FOLDERS="fish,git,gnupg,isort,karabiner,kitty,nvim,ripgrep,vim,ipython"
-
-for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
-do
-    stow $folder
-done
+stow fish
+stow git
+stow gnupg
+stow ipython
+stow karabiner
+stow kitty
+stow nvim
+stow ripgrep
