87b7cbd24a8e8e8fe76ccf283a3cd8b9a8ab571d
Denys Kolomytskyi <kolomytskyi@gmail.com>
2024-12-21 00:40:34 +0200

    Better tide colors

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,6 +8,9 @@ if status is-interactive
     set --export GPG_TTY (tty)
 
     set --global tide_right_prompt_items status cmd_duration context jobs direnv python go terraform
+    set --global tide_pwd_color_dirs 51657b
+    set --global tide_pwd_color_anchors 7e97ab
+    set --global tide_git_color_branch 578266
 
     pyenv init --path | source
     pyenv init - | source
