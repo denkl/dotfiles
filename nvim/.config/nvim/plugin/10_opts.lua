f07ac198e268da3186c2c07f6ddd164d1d1c4799
Denys Kolomytskyi <kolomytskyi@gmail.com>
2025-12-18 18:57:15 +0200

    update opts

--- nvim/.config/nvim/plugin/10_opts.lua
+++ nvim/.config/nvim/plugin/10_opts.lua
@@ -1,29 +1,28 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.o.breakindent = true -- visually indent wrapped lines when wrap is set
-vim.o.showmode = false -- do not show mode message
-vim.o.splitbelow = true -- put the new window below the current one
-vim.o.splitright = true -- put the new window right of the current one
--- vim.o.title = true -- change the terminal title according to the currently active buffer
-vim.o.wrap = false -- do not wrap long lines
-vim.o.cursorline = true -- show which cursor is on
-vim.o.number = true -- precede each line with its line number
+vim.o.breakindent = true    -- visually indent wrapped lines when wrap is set
+vim.o.cursorline = true     -- show which cursor is on
+vim.o.expandtab = true      -- replace tabs with spaces
+vim.o.ignorecase = true     -- ignore case in search patterns
+vim.o.number = true         -- precede each line with its line number
+vim.o.pumheight = 10        -- smaller popup menu
 vim.o.relativenumber = true -- show the line number relative to the line with the cursor
-vim.o.scrolloff = 5 -- minimal number of screen lines to keep above and below the cursor
-vim.o.signcolumn = "yes" -- always draw the signcolumn
-
-vim.o.completeopt = "menuone,noselect,fuzzy,nosort" -- options for Insert mode completion
-vim.o.expandtab = true -- replace tabs with spaces
-vim.o.ignorecase = true -- ignore case in search patterns
-vim.o.shiftwidth = 4 -- number of spaces to use for each step of (auto)indent
-vim.o.smartcase = true -- match case only when at least one char is upper
-vim.o.smartindent = true -- make indenting smart
-vim.o.tabstop = 4 -- number of spaces to a <Tab>
-vim.o.undofile = true -- enable persistent undo
+vim.o.scrolloff = 5         -- minimal number of screen lines to keep above and below the cursor
+vim.o.shiftwidth = 4        -- number of spaces to use for each step of (auto)indent
+vim.o.showmode = false      -- do not show mode message
+vim.o.signcolumn = "yes"    -- always draw the signcolumn
+vim.o.smartcase = true      -- match case only when at least one char is upper
+vim.o.smartindent = true    -- make indenting smart
+vim.o.splitbelow = true     -- put the new window below the current one
+vim.o.splitright = true     -- put the new window right of the current one
+vim.o.tabstop = 4           -- number of spaces to a <Tab>
+vim.o.undofile = true       -- enable persistent undo
 vim.o.virtualedit = 'block' -- allow going past the end of line in visual block mode
-vim.o.writebackup = false -- do not make backup before overwriting a file
-
+vim.o.winborder = 'single'  -- single-line box border style of floating windows
+vim.o.wrap = false          -- do not wrap long lines
+vim.o.writebackup = false   -- do not make backup before overwriting a file
+vim.o.completeopt = "menuone,noselect,fuzzy,nosort" -- options for Insert mode completion
 
 vim.api.nvim_create_autocmd("TextYankPost", {
     group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
