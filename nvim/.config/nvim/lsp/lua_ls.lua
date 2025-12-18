a3fef7766bf092afe8142dc807722f39135b018a
Denys Kolomytskyi <kolomytskyi@gmail.com>
2025-12-18 17:37:10 +0200

    override only needed lua_ls settings

--- nvim/.config/nvim/lsp/lua_ls.lua
+++ nvim/.config/nvim/lsp/lua_ls.lua
@@ -1,11 +1,7 @@
 vim.lsp.config.lua_ls = {
-    cmd = { 'lua-language-server' },
-    filetypes = { 'lua' },
     root_markers = { '.git' },
     settings = {
         Lua = {
-            codeLens = { enable = true },
-            hint = { enable = true, semicolon = 'Disable' },
             workspace = {
                 library = vim.api.nvim_get_runtime_file('', true),
             },
