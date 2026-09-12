39186d530152730c620c9207f05228d85cbb6034
Denys Kolomytskyi <kolomytskyi@gmail.com>
2026-09-12 11:03:37 +0300

    remove raycast

--- brew.sh
+++ brew.sh
@@ -55,7 +55,6 @@ brew install --cask font-fira-code
 brew install --cask karabiner-elements
 brew install --cask kitty
 brew install --cask ngrok
-brew install --cask raycast
 brew install --cask vlc
 
 brew cleanup
